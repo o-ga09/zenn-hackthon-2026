4dd6a474df41805ba1073e1f642eb322b5e57c1a
o-ga09 <taitiabe1997@gmail.com>
2026-02-11 21:48:37 +0900

    feat: CORSポリシーのヘッダーを追加しました 🛡️

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -37,6 +37,23 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin-allow-popups',
+          },
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'require-corp',
+          },
+        ],
+      },
+    ]
+  },
 }
 
 export default nextConfig
