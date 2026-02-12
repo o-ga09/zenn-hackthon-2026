29b2dca437e3f3558e5ad5d1a2fa0a9e081dcdc4
o-ga09 <taitiabe1997@gmail.com>
2026-02-12 23:00:08 +0900

    feat: 環境に応じたアセットプレフィックスを設定しました 📦

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   output: 'standalone',
+  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://static-agent.tavinikkiy.com' : '',
   /* config options here */
   images: {
     remotePatterns: [
